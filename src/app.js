9b1e3bbd64c739dedb3054bf3716d7725224a6f5
Aditya Bhosale <adityabhosale4388@gmail.com>
2025-12-12 23:40:02 +0530

    Fix app.js logic and update config

--- src/app.js
+++ src/app.js
@@ -21,11 +21,23 @@ import healthcheckRouter from "./routes/healthcheck.routes.js"
 import userRouter from "./routes/user.routes.js"
 import { errorHandler } from "./middleware/error.middleware.js"
 import videoRouter from "./routes/video.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
 
 //routes
 app.use("/api/v1/healthcheck",healthcheckRouter) //middleware
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/tweet",tweetRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/playlists", playlistRouter)    
 
 
 app.use(errorHandler)
